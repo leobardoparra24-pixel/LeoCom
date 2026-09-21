d8a598ac05367e984e4a9e8d0134c20d99da540b
leobardoparra24-pixel <leobardoparra24@gmail.com>
2026-09-20 19:36:12 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -34,7 +34,7 @@ def upload(
     data_dir=Path(os.getenv("DATA_DIR","/tmp"))/"uploads"; data_dir.mkdir(parents=True,exist_ok=True)
     path=data_dir/f"{uuid.uuid4().hex}{ext}"
     with path.open("wb") as f: shutil.copyfileobj(file.file,f)
-    job = Job(
+job = Job(
     filename=xmlfile.filename,
     stored_path=str(path),
     aux_file=str(aux_path)
